5346e3223247862f9a93fb5fd7bd73f899621045
Sean LeCheminant <32964349+saltytostitos@users.noreply.github.com>
2025-04-28 23:01:31 -0500

    added tw only

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -52,6 +52,30 @@ useSeoMeta({
       <UInput type="text" class="w-28" placeholder="text" />
       <USelect class="w-28" placeholder="select" />
     </div>
+    <UAlert
+      description="Tailwind Only"
+      class="max-w-lg mx-auto mt-6"
+      variant="subtle"
+    />
+    <div class="flex gap-2 justify-center mt-6 border-y border-muted">
+      <input
+        type="search"
+        class="w-28 rounded-md border-0 placeholder:text-dimmed focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 transition-colors px-2.5 py-1.5 text-sm gap-1.5 text-highlighted bg-default ring ring-inset ring-accented focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-primary"
+        placeholder="search"
+      />
+      <input
+        type="text"
+        class="w-28 rounded-md border-0 placeholder:text-dimmed focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 transition-colors px-2.5 py-1.5 text-sm gap-1.5 text-highlighted bg-default ring ring-inset ring-accented focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-primary"
+        placeholder="text"
+      />
+      <button
+        role="combobox"
+        type="button"
+        class="w-28 relative group rounded-md inline-flex items-center focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 transition-colors px-2.5 py-1.5 text-sm gap-1.5 text-highlighted bg-default ring ring-inset ring-accented focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-primary pe-9"
+        placeholder="select"
+      />
+    </div>
+
     <ContentRenderer v-if="page" :value="page" :prose="false" />
   </div>
 </template>
